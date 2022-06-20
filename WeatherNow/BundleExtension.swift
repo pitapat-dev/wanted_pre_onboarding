98ae38b21eb121a10900b82905635871e8409e75
Kei <vvv.devk@gmail.com>
2022-06-20 22:45:20 +0900

    Add: WeatherAPIKey.plist added

--- WeatherNow/BundleExtension.swift
+++ WeatherNow/BundleExtension.swift
@@ -11,7 +11,7 @@ extension Bundle {
     var apiKey: String {
         guard let file = self.path(forResource: "WeatherAPIKey", ofType: "plist") else { return "" }
         guard let resource = NSDictionary(contentsOfFile: file) else { return ""}
-        guard let key = resource["API_KEY"] as? String else { fatalError("API_KEY를 확인해주세요.")}
+        guard let key = resource["API_KEY"] as? String else { fatalError("WeatherAPIKey.plist에 API key를 입력해주세요.")}
         return key
     }
 }
