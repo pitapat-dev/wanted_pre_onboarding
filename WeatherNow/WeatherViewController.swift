1d019a5c2fe181404fb9c8d2fba7d38743d621ac
Kei <vvv.devk@gmail.com>
2022-06-20 15:03:49 +0900

    Add: First Scene UI Components Added.

---
+++ WeatherNow/WeatherViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherViewController.swift
+//  WeatherNow
+//
+//  Created by CHUBBY on 2022/06/20.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+}
